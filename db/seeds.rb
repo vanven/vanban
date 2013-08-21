24f447f8c683ade36713b96e3d2d869093883c5a
Vanessa Vengco <vengco@gmail.com>
2013-08-20 18:36:47 -0700

    Seed database with some test users and their kanbans

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(
+  name: 'Mortimer',
+  provider: 'google_oauth2',
+  uid: '1111',
+  email: 'koala4lyf@gmail.com',
+  kanbans: [
+    Kanban.new(board_name: 'My Kanban')
+  ]
+)
+
+User.create!(
+  name: 'Hans',
+  provider: 'google_oauth2',
+  uid: '1234',
+  email: 'hans.koala@gmail.com',
+  kanbans: [
+    Kanban.new(board_name: 'Project 1'),
+    Kanban.new(board_name: 'Project 2'),
+    Kanban.new(board_name: 'Project 3')
+  ]
+)
