f8d6cc8d3d8c2cde5281e9c33b6253722ce84b9f
Vanessa Vengco <vengco@gmail.com>
2013-08-20 18:39:00 -0700

    Add/begin RSpec specs for kanban and user models

--- spec/models/kanban_spec.rb
+++ spec/models/kanban_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Kanban do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:kanban) {Kanban.new}
+
+  describe 'attributes' do
+    it 'belongs to a user' do
+      expect(kanban.user).to eql nil
+    end
+
+    it 'has a name' do
+      kanban.board_name = 'My Awesome Kanban!'
+
+      expect(kanban.board_name).to eql 'My Awesome Kanban!'
+    end
+
+    # it 'has panels' do
+    # end
+
+    # it 'has many blocks' do
+    #   kanban.blocks = 'Some block title' Wait. What? TO_DO fix this
+
+
+    #   expect(kanban.block).to eql 'Some block title'
+    # end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new }
+  # let(:)
+
+  describe 'attributes' do
+    it 'has a name' do
+      user.name = 'Mortimer'
+
+      expect(user.name).to eql 'Mortimer'
+    end
+
+    # it 'has an email' do
+    #   user.email = 'mortimer@koala.com'
+
+    #   expect(user.email).to eql 'mortimer@koala.com'
+    # end
+
+    # it 'has kanbans' do
+    #   user.kanban =
+    # end
+  end
 end
